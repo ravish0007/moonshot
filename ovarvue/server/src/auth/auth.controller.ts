98cbf656c15cc3514b2110e72d4ce84c865be05d
ravish0007 <ravish.nfs@gmail.com>
2024-12-27 11:12:43 +0530

    add aggregate route

--- ovarvue/server/src/auth/auth.controller.ts
+++ ovarvue/server/src/auth/auth.controller.ts
@@ -2,23 +2,24 @@ import { Controller, Get, Post, UseGuards, Req, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup-dto';
 import { SignInDto } from './dto/signin-dto';
-import { AuthGuard } from './auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('signin')
   signin(@Body() dto: SignInDto) {
     return this.authService.validateUser(dto.email, dto.password);
   }
 
-  @UseGuards(AuthGuard)
   @Get('me')
   me(@Req() req) {
     return this.authService.me(req.user);
   }
 
+  @Public()
   @Post('signup')
   signup(@Body() dto: SignUpDto) {
     return this.authService.addUser(dto);
